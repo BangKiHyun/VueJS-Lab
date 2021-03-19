1b42540bca4344e7c9d93aaff671cceb22089480
BangKiHyun <rlrlvh@naver.com>
2021-03-19 21:14:29 +0900

    feat: enter 입력시 검색 결과 보여줌

--- searchform/1-vanilla/js/views/FormView.js
+++ searchform/1-vanilla/js/views/FormView.js
@@ -6,8 +6,8 @@ const FormView = Object.create(View)
 
 FormView.setup = function (el) {
     this.init(el)
-    this.inputEl = el.querySelector('[type-text')
-    this.resetEl = el.querySelector('[type-reset]')
+    this.inputEl = el.querySelector('[type=text]')
+    this.resetEl = el.querySelector('[type=reset')
     this.showResetBtn(false)
     this.bindEvents()
     return this
@@ -18,11 +18,16 @@ FormView.showResetBtn = function (show = true) {
 }
 
 FormView.bindEvents = function () {
+    this.on('submit', e => e.preventDefault())
     this.inputEl.addEventListener('keyup', e => this.onKeyup(e))
 }
 
-FormView.onKeyup = function () {
+FormView.onKeyup = function (e) {
+    // enter 키는 key 값이 13으로 정의되어 있음
+    const enter = 13
     this.showResetBtn(this.inputEl.value.length)
+    if (e.keyCode !== enter) return
+    this.emit('@submit', { input: this.inputEl.value })
 }
 
 export default FormView
