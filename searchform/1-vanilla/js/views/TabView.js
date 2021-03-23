a872c7639ec2326869a6cf96afe27b2adbcb0bf8
BangKiHyun <rlrlvh@naver.com>
2021-03-23 16:49:41 +0900

    feat: 각 탭 클릭시 탭 아래 내용 변경

--- searchform/1-vanilla/js/views/TabView.js
+++ searchform/1-vanilla/js/views/TabView.js
@@ -12,6 +12,18 @@ TabView.setActiveTab = function (tabName) {
     Array.from(this.el.querySelectorAll('li')).forEach(li => {
         li.className = li.innerHTML === tabName ? 'active' : ''
     })
+    this.show()
+}
+
+TabView.bindClick = function () {
+    Array.from(this.el.querySelectorAll('li')).forEach(li => {
+        li.addEventListener('click', e => this.onClick(li.innerHTML))
+    })
+}
+
+TabView.onClick = function (tabName) {
+    this.setActiveTab(tabName)
+    this.emit('@change', {tabName})
 }
 
 export default TabView
