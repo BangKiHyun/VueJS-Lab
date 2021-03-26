806961cd18be95ff139d9bf11e46d0460b485005
BangKiHyun <rlrlvh@naver.com>
2021-03-26 23:39:51 +0900

    feat: 추천 목록에서 검색어 클릭시 선택된 검색어로 결과 화면을 이동

--- searchform/1-vanilla/js/views/ResultView.js
+++ searchform/1-vanilla/js/views/ResultView.js
@@ -4,13 +4,18 @@ const tag = '[ResultView]'
 
 const ResultView = Object.create(View)
 
+ResultView.messages = {
+    NO_RESULT: '검색 결과가 없습니다'
+}
+
 ResultView.setup = function (el) {
     this.init(el)
 }
 
 ResultView.render = function (data = []) {
     console.log(tag, 'render()', data)
-    this.el.innerHTML = data.length ? this.getSearchResultsHtml(data) : '검색 결과가 없습니다'
+    this.el.innerHTML = data.length ? this.getSearchResultsHtml(data) : this.messages.NO_RESULT
+    this.show()
 }
 
 ResultView.getSearchResultsHtml = function (data) {
