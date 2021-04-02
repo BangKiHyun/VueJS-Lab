6cb72f4b28d61a75a2e7e296e76d8067008f89a4
BangKiHyun <rlrlvh@naver.com>
2021-04-02 22:02:14 +0900

    feat: 검색 결과 보여주는 기능 추가

--- searchform/2-vue/js/app.js
+++ searchform/2-vue/js/app.js
@@ -1,3 +1,5 @@
+import SearchModel from "./models/SearchModel.js";
+
 new Vue({
     // element를 의미하는 el
     // vue index가 어디에 바이딩될지 선택
@@ -5,10 +7,12 @@ new Vue({
     data: {
         //입력 데이터 받아서 저장할 data
         query: '',
+        submitted: false,
+        searchResult: []
     },
     methods: {
         onSubmit(e) {
-            debugger
+            this.search()
         },
         onKeyup() {
             if (!this.query.length) this.onReset()
@@ -17,6 +21,12 @@ new Vue({
             this.query = ''
             // 검색 결과 숨기는 로직
             debugger
-        }
+        },
+        search() {
+            SearchModel.list().then(data => {
+                this.submitted = true
+                this.searchResult = data
+            })
+        },
     }
 })
