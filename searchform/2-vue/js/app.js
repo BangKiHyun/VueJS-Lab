d92036da089c87ff22331033ee5f0b90edd4406f
BangKiHyun <rlrlvh@naver.com>
2021-04-02 21:06:32 +0900

    feat: 검색 상품명 입력 폼 추가

--- searchform/2-vue/js/app.js
+++ searchform/2-vue/js/app.js
@@ -1,8 +1,9 @@
 new Vue({
     // element를 의미하는 el
-    // vue index가 어디에 매치될지 선택
+    // vue index가 어디에 바이딩될지 선택
     el: '#app',
     data: {
-        msg: 'hello world'
+        //입력 데이터 받아서 저장할 data
+        query: ''
     }
 })
