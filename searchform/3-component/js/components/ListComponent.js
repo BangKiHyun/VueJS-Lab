8e58ee491629a5121c1d3a39c65d37444bfb8921
BangKiHyun <rlrlvh@naver.com>
2021-04-06 16:48:21 +0900

    feat: ListComponent를 통한 추천 검색어 및 최근 검색어 목록 출력

---
+++ searchform/3-component/js/components/ListComponent.js
@@ -0,0 +1,13 @@
+export default {
+    template: '#list',
+    props: ['data', 'type'],
+
+    methods: {
+        onClickList(keyword) {
+            this.$emit('@click', keyword)
+        },
+        onClickRemoveList(keyword) {
+            this.$emit('@remove', keyword)
+        }
+    }
+}
